618dedad92e94b09fc1e2f30a79eecc3815b03d8
Alisson Chiquitto <chiquitto@gmail.com>
2016-05-29 09:47:57 -0300

    SoulShop config

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 # Add any extra stuff you might need here.
+
+# cd ~
+pwd
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+sudo mv phpunit.phar /usr/local/bin/phpunit
+phpunit --version
+# cd -
