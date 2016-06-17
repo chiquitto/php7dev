fb997664b901cc9797ee26666ad033b6b968334e
Alisson Chiquitto <chiquitto@gmail.com>
2016-06-17 10:41:42 -0300

    Commit Marknet

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
 # Add any extra stuff you might need here.
-
-# cd ~
-pwd
-wget https://phar.phpunit.de/phpunit.phar
-chmod +x phpunit.phar
-sudo mv phpunit.phar /usr/local/bin/phpunit
-phpunit --version
-# cd -
---
+++ scripts/phpunit-install.sh
@@ -0,0 +1,7 @@
+cd ~
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+cp phpunit.phar /usr/local/bin/phpunit
+cp phpunit.phar /usr/local/bin/phpunit.phar
+phpunit --version
+cd -
